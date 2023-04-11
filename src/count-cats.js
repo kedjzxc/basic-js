07ad83f139fdcc27ee1b3d8e0e1a970e2f6734fa
kedjzxc <hailrake27@gmail.com>
2023-04-11 12:51:13 +0300

    fix: fix countcats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
+function countCats(arr) {
   let newArr = arr.flat(Infinity)
   let catsNum = 0
   newArr.forEach(cat => {
