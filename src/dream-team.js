e334f19ce71c7269dcab387631b93f6b6096b042
kedjzxc <hailrake27@gmail.com>
2023-04-12 23:43:59 +0300

    new commit

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,8 +13,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  let teamName = '';
+  members.forEach(item => {
+    if (typeof item == "string") {
+      teamName += item.slice(0,1)
+    }
+  })
+  return teamName.split('').sort().join('')
   // remove line with error and write your code here
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,8 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!'
+  }
+  const newDate = Date.parse(date)
+  const dateObj = new Date(newDate)
+  return dateObj.getMonth() == 2 || dateObj.getHours() == 3 || dateObj.getHours() == 4 ? 'spring' :
+        dateObj.getMonth() == 5 || dateObj.getHours() == 6 || dateObj.getHours() == 7 ? 'summer' : 
+        dateObj.getMonth() == 8 || dateObj.getHours() == 9 || dateObj.getHours() == 10 ? 'autumn' : 
+        dateObj.getMonth() == 11 || dateObj.getHours() == 12 || dateObj.getHours() == 0 ? 'winter' : '' 
   // remove line with error and write your code here
 }
 
